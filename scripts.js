49420053f20877b2ab7dba5c1c5a77784ca3603a
Oscar Li <lioscar58@gmail.com>
2024-01-14 17:45:34 -0500

    updated styles

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     var line2 = document.getElementById('line2');
     var input = document.getElementById('input');
+    var enterButton = document.getElementById('enterButton');
   
     setTimeout(function() {
       line2.style.display = 'none';
       input.style.display = 'block';
     }, 10000); // Adjust this value to match the duration of your CSS animations
   
-    input.addEventListener('keydown', function(e) {
-      if (e.key === 'Enter' && input.value.toLowerCase() === 'yes') {
-        window.location.href = 'landing.html'; 
+    input.addEventListener('input', function(e) {
+      if (input.value.toLowerCase() === 'yes') {
+        enterButton.style.display = 'block';
+      } else {
+        enterButton.style.display = 'none';
       }
+    });
+  
+    enterButton.addEventListener('click', function() {
+      window.location.href = 'landing.html';
     });
   });
